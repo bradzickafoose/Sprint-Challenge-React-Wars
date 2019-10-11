07a29d668d2eb17dca5ba287d8487f54b8ad62e8
Brad <brad@viziworx.com>
2019-10-11 13:40:16 -0400

    Added animation to Character

--- starwars/src/components/CharacterInfo.js
+++ starwars/src/components/CharacterInfo.js
@@ -1,16 +1,30 @@
 import React from "react";
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
+
+const pulse = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(1.025);
+  }
+`;
 
 const Character = styled.div`
     padding: 1.5rem;
     background-color: #fff;
     border-radius: .5rem;
     box-shadow: 0 10px 15px -3px rgba(0,0,0,.1),0 4px 6px -2px rgba(0,0,0,.05);
+    animation: ${pulse} 0.2s ease-in-out infinite alternate;
+    animation-play-state: paused;
+    &:hover {
+      animation-play-state: running;
+    }
 `;
 
 const CharacterName = styled.h2`
     margin-top: 0;
-    color: #4f4a4a;
+    color: #6c4c33;
 `;
 
 const CharacterStats = styled.div`
@@ -20,7 +34,7 @@ const CharacterStats = styled.div`
 `;
 
 const CharacterStat = styled.span`
-    color: #aeaeae;
+    color: #937157;
     font-size: .8rem;
 
     span {
