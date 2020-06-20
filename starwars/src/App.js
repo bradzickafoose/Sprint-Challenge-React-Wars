67c7248d24f60f06aa619495605c0bb0e222129d
Brad <brad@viziworx.com>
2020-06-20 13:25:27 -0400

    moves state to app, replaces axios with fetch

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import Characters from './components/Characters';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import endpoint from "./endpoint";
+
+import Characters from "./components/Characters";
 
 const App = () => {
+  const [characters, setCharacters] = useState([]);
 
-  return (
-    <div className='App'>
-      <h1 className='Header'>React Wars</h1>
-      <Characters />
-    </div>
-  );
-}
+  useEffect(() => {
+    fetch(endpoint + '/people')
+      .then(response => response.json())
+      .then(response => {
+        const characters = response.results
+        console.log('React Wars characters:', characters)
+        setCharacters(characters)
+      })
+      .catch(error => console.log('Error retrieving characters data:', error));
+  }, []);
+
+    return (
+        <div className="App">
+            <header className="Header">
+                <h1>React Wars</h1>
+            </header>
+            <Characters characters={characters} />
+        </div>
+    );
+};
 
 export default App;
--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -1,24 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Character from './Character';
+import React from "react";
+import Character from "./Character";
 
-export default function CharactersList() {
-    const [characters, setCharacters] = useState([]);
-
-    useEffect(() => {
-        axios
-          .get('https://swapi.py4e.com/api/people/')
-          .then(result => {
-            const characters = result.data.results
-            console.log('React Wars characters:', characters)
-            setCharacters(characters)
-          })
-          .catch(error => console.log('Error retrieving characters data:', error));
-    }, []);
-
+const Characters = ({ characters = [] }) => {
     return (
-        <section className='characters'>
-            {characters.map(character => <Character key={character.created} {...character} /> )}
+        <section className="characters">
+            {characters.map(character => (
+                <Character key={character.created} {...character} />
+            ))}
         </section>
     );
-}
+};
+
+export default Characters;
